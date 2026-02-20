c8bf5464c4f51ce3f3ec2d263f0da12986af8b67
Rastvorov Sergei <144595454+krevetka-is-afk@users.noreply.github.com>
2026-02-20 12:07:39 +0300

    scala first meet

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,16 +1,21 @@
+import org.apache.spark.sql.SparkSession
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 object Main {
   def main(args: Array[String]): Unit = {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    (1 to 5).map(println)
+    val spark = SparkSession.builder()
+      .appName("demo")
+      .master("local[*]")
+      .getOrCreate()
 
-    for (i <- 1 to 5) {
-      //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-      // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-      println(s"i = $i")
-    }
+    import spark.implicits._
+
+    val df = Seq(
+      ("a", 1),
+      ("b", 2),
+      ("a", 3)
+    ).toDF("k", "v")
+    df.groupBy("k").sum("v").show()
+
+    spark.stop()
   }
 }
-
